5c415ce30544016ff04122c34b3c576f009a9b0b
therealdanmalmx <danmalmx@gmail.com>
2022-04-02 22:50:51 +0200

    Add dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-4xl bold mt-4">Squared Circle Studio</h1>
+      <h1 className="text-center text-4xl bold mt-4 text-green-900">
+        Squared Circle Studio
+      </h1>
     </div>
   );
 }
