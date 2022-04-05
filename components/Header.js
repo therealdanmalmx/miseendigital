ab4c184847517f0b5c87d72b3b4f464e7ede6ec6
therealdanmalmx <danmalmx@gmail.com>
2022-04-05 21:42:19 +0200

    Add header with animation

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,44 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Header = () => {
-  return <div className="text-center mt-12">Header</div>;
+  return (
+    <div className="mt-4">
+      <motion.div
+        className="text-center"
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.75 }}
+      >
+        <div>
+          <div className="flex justify-center">
+            <p>Hello</p>
+            <span style={{ marginLeft: 8, marginRight: 12 }}>😊</span>
+            <p>we are</p>
+          </div>
+          <div></div>
+        </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.75, delay: 0.25 }}
+        className="text-center"
+      >
+        <p className="text-6xl text-slate-600 li">Squared Circle Studio</p>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.75, delay: 0.6 }}
+        className="text-center"
+      >
+        <div className="flex justify-center text-lg font-bold  text-slate-800">
+          <p className="mx-3">Ecommerce</p>
+          <p className="mx-3">Web apps</p>
+          <p className="mx-3">Institutional sites</p>
+          <p className="mx-3">Web Animations</p>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
