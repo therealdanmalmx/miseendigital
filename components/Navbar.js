e7935dc41c99497298aa6da528099ea9a1bf1588
Sérgio Costa <costa.sergio.86@gmail.com>
2022-04-09 00:07:32 +0100

    Add About Section and Change background in Header Scroll

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,30 @@
-import Image from 'next/image';
 import { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-import { images } from "./../utils";
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    console.log(isScrolled)
+    
+    const changeBackground = () => {
+        if(window.scrollY >= 80) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground)
+
   return (
-    <nav className="app__navbar fixed w-full h-[80px] border-b-[1px] border-white flex justify-between items-center px-8 py-14 backdrop-blur-sm z-10">
+    <nav className={`app__navbar fixed top-0 left-0 transition-all duration-200 w-full h-[80px] border-b-[1px] border-white flex justify-between items-center px-8 py-14 backdrop-blur-sm z-10
+    ${isScrolled ? "bg-white" : "bg-transparent"}`} >
         <div className="app__navbar-logo flex justify-start items-center ">
-            <h1 className="text-3xl text-white font-bold mr-4 sm:text-4xl">Brand.</h1>
+            <h1 className={`text-3xl text-white font-bold mr-4 sm:text-4xl ${isScrolled ? "text-black" : "text-white"}`} >Brand.</h1>
         </div>
         <div className="flex items-center">
-            <div class="text-white font-bold mr-4">Menu</div> 
+            <div class={`text-white font-bold mr-4 ${isScrolled ? "text-black" : "text-white"}`}>Menu</div> 
             <div className="app__navbar-menu cursor-pointer w-9 h-9 relative flex justify-center items-center bg-primary-color"> 
                 <HiMenuAlt4 className="text-white w-6 h-6" onClick={() => setToggle(true)} />
 
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { images } from "../../utils";
+
+const abouts = [
+  { title: 'Marketing Digital', description:'I am a good developer', imgUrl: ''},
+  { title: 'Branding', description:'I am a good developer', imgUrl:''},
+  { title: 'Web Design', description:'I am a good developer', imgUrl:''},
+  { title: 'Web Development', description:'I am a good developer', imgUrl:''},
+  { title: 'Mobile Development', description:'I am a good developer', imgUrl:''},
+  { title: 'E-commerce', description:'I am a good developer', imgUrl:''}
+]
+
 const About = () => {
-  return <div className="text-center">About</div>;
+  return (
+    <>
+      <h1 className="app_about-head-text flex-1 w-full flex-col text-center pt-20 font-bold md:text-5xl">
+          We help your business grow  
+      </h1>
+        <div className="app__profiles flex justify-center items-start flex-wrap mt-8">
+            {abouts.map((about, index) =>(
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.5, type: 'tween'}}
+                className="app__profile-item w-[260px] md:w-[400px] md:h-[300px] flex flex-col items-start justify-start m-5"
+                key={about.title + index}
+              >
+                <Image className="w-full rounded-xl object-cover" src={images.services} alt={about.title} />
+                <h2 className="bold-text font-bold" style={{ marginTop: 20}}>{about.title}</h2>
+                <p className="p-text" style={{ marginTop: 5}}>{about.description}</p>
+              </motion.div>
+            ))}
+        </div> 
+      </>  
+  );
 };
 
 export default About;
