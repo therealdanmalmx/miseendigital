67fc5560a0a5c3e1b77273a02592b68338f0e593
therealdanmalmx <danmalmx@gmail.com>
2022-04-09 21:32:42 +0200

    Add transition to button

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,16 @@
 const Hero = () => {
   return (
-    <div class="app_hero w-full h-screen bg-secondary-color flex flex-col justify-center items-center">
-        <h1 className="mb-4 text-white font-bold text-5xl text-center leading-snug md:text-7xl max-w-4xl md:leading-tight">
-            We <span className="text-primary-color">design</span> and <span className="text-primary-color">develop</span> digital solutions
-        </h1>
-        <p className="text-white text-2xl mb-8">Based remotely, with clients all over the world</p>
-        <button>Our Portfolio</button>
+    <div class="app_hero flex h-screen w-full flex-col items-center justify-center bg-secondary-color">
+      <h1 className="mb-4 max-w-4xl text-center text-5xl font-bold leading-snug text-white md:text-7xl md:leading-tight">
+        We <span className="text-primary-color">design</span> and{" "}
+        <span className="text-primary-color">develop</span> digital solutions
+      </h1>
+      <p className="mb-8 text-2xl text-white">
+        Based remotely, with clients all over the world
+      </p>
+      <button className="duration-300 ease-in-out">Our Portfolio</button>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
