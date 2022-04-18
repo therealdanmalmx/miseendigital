79fc6e0799c58a3b54e0ed88768f311c6c61e0b6
therealdanmalmx <danmalmx@gmail.com>
2022-04-18 21:49:05 +0200

    Add hardcode projectId. Not accepting env.

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.NEXT_APP_SANITY_PROJECT_ID,
+  projectId: "l287glsi",
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
