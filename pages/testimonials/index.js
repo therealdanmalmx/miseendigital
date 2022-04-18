d65e97d856122656fec67d2c71e84f9a878ae606
therealdanmalmx <danmalmx@gmail.com>
2022-04-18 21:21:43 +0200

    Add image (WIP) to Testimonials

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -15,15 +15,30 @@ const Testimonial = () => {
   return (
     <div>
       <h2 className="mt-28 mb-2 text-center">Testimonials</h2>
-      {testimonials.map((testimonial, index) => (
-        <div key={testimonial._id}>
-          <div className="text-center">
-            <h2>Feedback: {testimonial.feedback}</h2>
-            <p>Name: {testimonial.name}</p>
-            <p>Company: {testimonial.company}</p>
-          </div>
-        </div>
-      ))}
+      {testimonials.map(
+        (testimonial, index) => (
+          console.log("testimonial", testimonial),
+          (
+            <div key={testimonial._id}>
+              <>
+                {/* <Image
+              key={index}
+              src={urlFor(testimonial.imgUrl)}
+              height={100}
+              width={100}
+              fill="objectFit"
+              className="mx-auto"
+            /> */}
+                <div className="text-center">
+                  <h2>Feedback: {testimonial.feedback}</h2>
+                  <p>Name: {testimonial.name}</p>
+                  <p>Company: {testimonial.company}</p>
+                </div>
+              </>
+            </div>
+          )
+        )
+      )}
     </div>
   );
 };
