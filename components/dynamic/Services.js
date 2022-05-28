742cae595ad9bbf223a06d4925c19a05118c1392
Sérgio Costa <costa.sergio.86@gmail.com>
2022-05-28 19:45:57 +0100

    Styling Service Details

--- components/dynamic/Services.js
+++ components/dynamic/Services.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import { urlFor } from "../../client";
 import Link from "next/link";
+import Footer from "../Footer"
 
 export default function Services({ abouts }) {
   const { title, description, imgUrl } = abouts;
@@ -15,23 +16,27 @@ export default function Services({ abouts }) {
     );
   } else {
     return (
+      <>
       <div className="flex flex-col items-center justify-center">
-        <div className="mt-40 text-center text-5xl font-bold">{title}</div>
-        <div className="mt-3 text-center text-xl font-bold">{description}</div>
-        <img
-          className=" relative mx-auto mt-5 h-48 w-full object-cover transition duration-300 ease-in-out hover:scale-105 md:h-60 md:w-72"
-          src={urlFor(imgUrl)}
-          alt={urlFor(imgUrl)}
-        />
         <Link href="/services">
           <button
             type="button"
-            className="mt-5 bg-primary-color text-lg text-white transition duration-300 ease-in-out hover:bg-black hover:text-white"
+            className="mt-40 mb-10 bg-primary-color text-lg text-white transition duration-300 ease-in-out hover:bg-black hover:text-white"
           >
             Back to services
           </button>
         </Link>
+        <div className=" text-center text-5xl font-bold">{title}</div>
+        <div className="mt-3 text-center text-xl font-bold">{description}</div>
+        <img
+          className="mb-10 relative mx-auto mt-5 h-[400px] w-full object-cover transition duration-300 ease-in-out md:h-[600px] md:w-[1000px]"
+          src={urlFor(imgUrl)}
+          alt={urlFor(imgUrl)}
+        />
       </div>
+
+      <Footer />
+      </>
     );
   }
 }
