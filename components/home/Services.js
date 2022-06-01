410504ae0ff90144bfd7515644de5bfc0e98f887
therealdanmalmx <danmalmx@gmail.com>
2022-06-01 21:02:45 +0200

    Add button link to services on home page

--- components/home/Services.js
+++ components/home/Services.js
@@ -1,9 +1,18 @@
+import Link from "next/link";
 const Services = () => {
   return (
-    <div className="app__works bg-light-color py-28">
-      <h1 className="app_about-head-text w-full flex-1 flex-col text-center font-bold md:text-5xl">
+    <div className="app__works flex-1 flex-col bg-light-color py-28 text-center">
+      <h1 className="app_about-head-text w-full font-bold md:text-5xl">
         Services
       </h1>
+      <Link href="/services">
+        <button
+          type="button"
+          className="mx-auto mt-5 bg-primary-color text-center text-white transition duration-300 ease-in-out hover:bg-black hover:text-white"
+        >
+          See all services
+        </button>
+      </Link>
     </div>
   );
 };
