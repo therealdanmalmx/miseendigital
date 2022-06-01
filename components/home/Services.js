289fc51b8a1d7f449e5e43cb35a58b09f1569229
therealdanmalmx <danmalmx@gmail.com>
2022-06-01 20:59:34 +0200

    Add services component to home page

---
+++ components/home/Services.js
@@ -0,0 +1,11 @@
+const Services = () => {
+  return (
+    <div className="app__works bg-light-color py-28">
+      <h1 className="app_about-head-text w-full flex-1 flex-col text-center font-bold md:text-5xl">
+        Services
+      </h1>
+    </div>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from "../components/base/Hero";
 import Works from "./works";
 import Skills from "../components/home/Skills";
 import Testimonial from "../components/home/Testimonials";
-import Services from "./services";
+import Services from "../components/home/Services";
 import Contacts from "../components/home/Contacts";
 
 export default function Home() {
