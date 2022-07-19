3de75d2c5a4b957b7ad9c9b26e65fb3d87524eae
therealdanmalmx <danmalmx@gmail.com>
2022-07-19 20:56:42 +0100

    Set text on top of images

--- components/pages/WorkPage.js
+++ components/pages/WorkPage.js
@@ -10,19 +10,19 @@ const  WorkPage = ({ work }) => {
     const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 });
 
   return (
-    <div>
+    <div className="flex">
         <motion.div
             animate={animateCard}
-            transition={{ duration: 0.5, delayChildren: 0.5 }}
-            className="app__work-portfolio flex w-full flex-1 flex-row flex-wrap items-center justify-center"
+            transition={{ duration: 0.5 }}
+            className="app__work-portfolio"
             >
                 <Link
                     passHref
                     href={`/works/${work.title.toLowerCase().split(" ").join("-")}`}
                 >
-                    <div className="app__work-img relative flex w-full items-center">
+                    <div className="app__work-img relative flex">
                         <img
-                            className="w-1/3 h-1/3 mx-10 my-5 rounded-lg border-2"
+                            className="w-1/3 h-1/3 mx-10 my-5"
                             src={urlFor(imgUrl)}
                             alt={work.name}
                         />
@@ -33,22 +33,19 @@ const  WorkPage = ({ work }) => {
                             ease: "easeInOut",
                             staggerChildren: 0.5,
                             }}
-                            className="app__work-hover absolute top-0 left-0  border-2  bottom-0 mx-10 my-5 cursor-pointer right-0 w-1/3 rounded bg-black bg-opacity-40 duration-300 ease-in"
+                            className="app__work-hover absolute top-0 left-0 bottom-0 mx-10 my-5 cursor-pointer right-0 w-1/3 rounded bg-black bg-opacity-40 duration-300 ease-in"
                         >
                         </motion.div>
-                            <div key={work._id}>
-                                <div>
-                                    <h4 className="bold-text absolute top-1/2 left-1/2 font-bold text-primary-color">
-                                    {work.title}
-                                    </h4>
-                                    <p className="text-center absolute" style={{ marginTop: 10 }}>
-                                    {work.description}
-                                    </p>
-                                    <div className="app__work-tag absolute top-10 rounded-lg bg-white py-2 px-4 text-base">
+                        <div key={work._id}>
+                            <div>
+                                <p className="top-20 left-16 absolute" style={{ marginTop: 10 }}>
+                                {work.description}
+                                </p>
+                                <div className="app__work-tag absolute top-10 left-16 bg-white py-2 px-4 text-base">
                                     <p className="">{work.tags[0]}</p>
-                                    </div>
                                 </div>
                             </div>
+                        </div>
                     </div>
                 </Link>
 
