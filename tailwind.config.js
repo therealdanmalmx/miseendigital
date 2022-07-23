3d459c4574491d933edcc3ee88ec4aacf381ae13
Sérgio Costa <costa.sergio.86@gmail.com>
2022-07-23 20:01:34 +0100

    Remove Screens from Tailwind; Add Grid to Testimonails;

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      'xxs': "375px",
-      // => @media (min-width: 357px) { ... }
-
-      'xs': "420px",
-      // => @media (min-width: 420px) { ... }
-
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
     colors: {
       'primary-color': '#0063ff',
       'secondary-color': '#080f20',
