1e198e7de738d66f061b77302f63e961bc904c55
therealdanmalmx <danmalmx@gmail.com>
2022-07-24 13:45:43 +0100

    Change opacity on background image

--- components/base/Hero.js
+++ components/base/Hero.js
@@ -3,7 +3,7 @@ import { images } from "../../utils";
 
 const Hero = () => {
   return (
-    <div className="app__hero h-[calc(100vh_-_113px)] w-full flex flex-col justify-center items-center bg-center bg-black bg-opacity-30 bg-blend-overlay" style={{ backgroundImage: `url(${images.communication.src})` }}>
+    <div className="app__hero h-[calc(100vh_-_113px)] w-full flex flex-col justify-center items-center bg-center bg-black bg-opacity-10 bg-blend-overlay" style={{ backgroundImage: `url(${images.communication.src})` }}>
       <h1 className="mb-4 max-w-4xl text-center text-5xl font-normal leading-snug text-white md:text-6xl md:leading-tight">
         We design, develop and{" "} modernize your digital tools
       </h1>
