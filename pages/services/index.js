b8d2131baf437ac493ba7f6bea1c5315a5641011
therealdanmalmx <danmalmx@gmail.com>
2022-07-26 21:16:02 +0100

    Add overlay with text

--- pages/services/index.js
+++ pages/services/index.js
@@ -37,33 +37,35 @@ const Services = () => {
         </p>
       </div>
       {/* Services overview */}
-      <div className="app__services flex flex-col items-center pt-20">
+      {/* <div className="app__services flex flex-col items-center pt-20">
         <h1 className="app__services-head-text h-96 mb-10 flex-1 flex-col text-center font-bold md:text-5xl">
           What we can do for you
         </h1>
         <p className="app__services-description-text mb-10 w-full px-7 text-center text-base md:w-6/12 md:text-lg">
           No matter what trends come and go, offering something that customers want, in the easiet way possible and with pleasing aesthetics will never go out of style. We focus on getting the basics right for you and your customers.
         </p>
-        </div>
+      </div> */}
         {/* Services items */}
-        <div className="app__profiles overflow-hidden flex flex-col cursor-pointer flex-wrap items-center">
+        <div className="app__profiles overflow-hidden flex flex-col py-20 cursor-pointer flex-wrap items-center">
           {abouts.map((about, index) => (
             <Link
               passHref
               href={`/services/${about.title.toLowerCase().split(" ").join("-")}`}
               key={about._id}
             >
-              <div className="relative overflow-hidden w-[325px] h-96 my-2 md:my-8 md:h-[450px] md:w-9/12">
+              <div className="relative overflow-hidden w-[325px] h-96 my-2 md:my-12 md:h-[450px] md:w-[1024px]">
                 {about.imgUrl && (
                   // eslint-disable-next-line @next/next/no-img-element
                   <img
                     src={`${urlFor(about.imgUrl)}`}
                     alt={about.title}
-                    className="ease-in-out duration-500 hover:scale-125 block relative w-full h-full object-cover"
+                    className="ease-in-out duration-500 hover:scale-125 peer-hover:scale-125 block relative w-full h-full object-cover"
                   />
                 )}
-                <div className="w-1/2 bg-black opacity-60 h-full"></div>
-                  <h2 className="bold-text bg-black px-4 py-2 absolute z-10 bottom-5 left-5 text-white font-bold">
+                  <div className="w-2/5 flex flex-col justify-center items-center peer text-sm leading-6 tracking-wide absolute top-0 bg-white right-0 opacity-80 bg-opacity-90 h-full">
+                    <p className="mx-10">{about.description}</p>
+                  </div>
+                  <h2 className="bold-text bg-black z-10 px-4 py-2 absolute bottom-5 left-5 text-white font-bold">
                     {about.title}
                   </h2>
               </div>
