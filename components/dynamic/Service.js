05f3bd3e684c97d7582664e3af11071c86f4e5a5
therealdanmalmx <danmalmx@gmail.com>
2022-07-30 16:47:25 +0100

    Adjust Service title for mobile and desktop

--- components/dynamic/Service.js
+++ components/dynamic/Service.js
@@ -40,7 +40,7 @@ export default function Service({ about }) {
             alt={urlFor(imgUrl)}
           />
         <div className="flex flex-col mx-10 ">
-          <div className="text-5xl py-2 px-4 text-white bg-black absolute bottom-40 font-bold">{title}</div>
+          <div className="text-3xl md:text-5xl py-2 px-4 text-white bg-black absolute bottom-40 font-bold">{title}</div>
           <div className="mt-3 absolute bottom-24 w-1/3 text-black text-xl font-semibold">{description}</div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 py-11">
@@ -61,31 +61,31 @@ export default function Service({ about }) {
           <div className="grid grid-cols-3 md:grid-cols-4 grid-rows-1 pb-10 md:pb-0">
                 <div className="row-span-2 justify-self-center self-center">
                   <Image
-                      src={images.design}
+                      src={images.develop}
                       width={80}
                       height={80}
-                      alt={images.design}
+                      alt={images.develop}
                   />
                 </div>
                 <div className="col-span-2 md:col-span-3">
-                  <h2 className="text-4xl font-bold pb-3">Design</h2>
+                  <h2 className="text-4xl font-bold pb-3">Develop</h2>
                   <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
                 </div>
-          </div> 
+          </div>
           <div className="grid grid-cols-3 md:grid-cols-4 grid-rows-1">
                 <div className="row-span-2 justify-self-center self-center">
                   <Image
-                      src={images.design}
+                      src={images.deploy}
                       width={80}
                       height={80}
-                      alt={images.design}
+                      alt={images.deploy}
                   />
                 </div>
                 <div className="col-span-2 md:col-span-3">
-                  <h2 className="text-4xl font-bold pb-3">Design</h2>
+                  <h2 className="text-4xl font-bold pb-3">Deploy</h2>
                   <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
                 </div>
-          </div>    
+          </div>
         </div>
 
       </div>
