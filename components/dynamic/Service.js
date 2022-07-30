1c2e1e5e3847af7840422d8300e67d2f651c72da
therealdanmalmx <danmalmx@gmail.com>
2022-07-30 18:10:56 +0100

    Add type of Services component.

--- components/dynamic/Service.js
+++ components/dynamic/Service.js
@@ -3,12 +3,13 @@ import { urlFor } from "../../client";
 import Link from "next/link";
 import { images } from "../../utils";
 import Image from 'next/image'
+import TypeOfServices from './TypeOfServices'
 
 import { HiOutlineChevronLeft } from "react-icons/hi";
 
 export default function Service({ about }) {
   console.log({about});
-  const { title, description, imgUrl } = about;
+  const { title, description, imgUrl, typeofservice } = about;
   const router = useRouter();
 
   if (router.isFallback) {
@@ -44,51 +45,59 @@ export default function Service({ about }) {
           <div className="mt-3 absolute bottom-24 w-1/3 text-black text-xl font-semibold">{description}</div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 py-11">
-          <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
-                <div className="row-span-2 justify-self-center self-center">
-                  <Image
-                      src={images.design}
-                      width={80}
-                      height={80}
-                      alt={images.design}
-                  />
-                </div>
-                <div className="col-span-2 md:col-span-2 lg:col-span-2">
-                  <h2 className="text-4xl font-bold pb-3">Design</h2>
-                  <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
-                </div>
-          </div> 
-          <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
-                <div className="row-span-2 justify-self-center self-center">
-                  <Image
-                      src={images.deploy}
-                      width={80}
-                      height={80}
-                      alt={images.deploy}
-                  />
-                </div>
-                <div className="col-span-2 md:col-span-2 lg:col-span-2">
-                  <h2 className="text-4xl font-bold pb-3">Design</h2>
-                  <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
-                </div>
-          </div> 
-          <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
-                <div className="row-span-2 justify-self-center self-center">
-                  <Image
-                      src={images.design}
-                      width={80}
-                      height={80}
-                      alt={images.design}
-                  />
-                </div>
-                <div className="col-span-2 md:col-span-2 lg:col-span-2">
-                  <h2 className="text-4xl font-bold pb-3">Design</h2>
-                  <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
-                </div>
-          </div> 
+          {typeofservice.map((service) => (
+            <TypeOfServices
+              key={service._key}
+              service={service}
+            />
+          ))}
         </div>
-
       </div>
-    );
-  }
-}
+        );
+      }
+    }
+    {/* <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 py-11">
+      <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
+            <div className="row-span-2 justify-self-center self-center">
+              <Image
+                  src={images.design}
+                  width={80}
+                  height={80}
+                  alt={images.design}
+              />
+            </div>
+            <div className="col-span-2 md:col-span-2 lg:col-span-2">
+              <h2 className="text-4xl font-bold pb-3">Design</h2>
+              <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
+            </div>
+      </div>  */}
+      {/* <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
+            <div className="row-span-2 justify-self-center self-center">
+              <Image
+                  src={images.deploy}
+                  width={80}
+                  height={80}
+                  alt={images.deploy}
+              />
+            </div>
+            <div className="col-span-2 md:col-span-2 lg:col-span-2">
+              <h2 className="text-4xl font-bold pb-3">Design</h2>
+              <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
+            </div>
+      </div>  */}
+      {/* <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 md:pb-10 lg:pb-0">
+            <div className="row-span-2 justify-self-center self-center">
+              <Image
+                  src={images.design}
+                  width={80}
+                  height={80}
+                  alt={images.design}
+              />
+            </div>
+            <div className="col-span-2 md:col-span-2 lg:col-span-2">
+              <h2 className="text-4xl font-bold pb-3">Design</h2>
+              <p className="w-full">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi autem ipsa dolore nesciunt doloribus eos id expedita ullam doloremque dicta!</p>
+            </div>
+      </div>
+    </div> */}
+
---
+++ components/dynamic/TypeOfServices.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import { urlFor } from "../../client";
+
+export default function TypeOfServices({ service }) {
+    console.log({service})
+    const { title, description, iconUrl } = service;
+    return (
+        <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 grid-rows-1 pb-10 mt-30 md:pb-10 lg:pb-0">
+            <div className="row-span-2 justify-self-center self-center">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    src={`${urlFor(iconUrl)}`}
+                    alt={urlFor(iconUrl)}
+                    width={80}
+                    height={80}
+                    />
+                </div>
+                <div className="col-span-2 md:col-span-2 lg:col-span-2">
+                    <h2 className="text-4xl font-bold pb-3">{ title }</h2>
+                    <p className="w-full">{ description }</p>
+                </div>
+        </div>
+    )
+}
